89df7fc369c98a9e36fb953e8eb081a461020c95
khalid3886 <khalid@flyo.ai>
2025-09-16 10:53:08 +0530

    added clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -125,6 +125,20 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tbi423zril");
+          `}
+        </Script>
+
         {/* Analytics Debugger for Development */}
         {process.env.NODE_ENV === 'development' && (
           <Script
